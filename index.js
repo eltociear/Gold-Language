72e35d95d98fa65984eb89d831c4f6f1023648aa
Jack Spencer <UniformKitten281@gmail.com>
2022-04-01 13:21:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
+// Code By Jack //
+
+// Import scripts and loaders from the ./modules folder. //
 import { goldLoad } from "./modules/interpreters/goldInt.js";
 import { copyContent } from './modules/scripts/copy.js';
 import { errorCodes } from "./modules/save/error.js";
 import { deleteContent } from "./modules/scripts/delete.js";
 import { goldShell } from "./modules/scripts/shell.js";
 
+// Import packages from NPM //
 import prompt from 'prompt';
 
+// Constants //
 const myGoldProgram = './program.gold';
 const destination = './modules/program.js';
 
+// Current Errors //
 var currentError = {
     code: 0,
     des: ""
 }
 
+// Load the Extension check //
 goldExtensionCheck();
 
+// Check for extensions from the myGoldProgram constant. //
 function goldExtensionCheck() {
 
     if (myGoldProgram.endsWith('.gold')) {
