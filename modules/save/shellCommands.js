000253a0c62d855fee4a48bedf62006c121bb8b2
Jack Spencer <UniformKitten281@gmail.com>
2022-04-01 21:47:15 -0700

    Create shellCommands.js

---
+++ modules/save/shellCommands.js
@@ -0,0 +1,32 @@
+const shellCommands = [
+    {
+        name: "help",
+        des: "> Search for help on any type of command."
+    },
+    {
+        name: "save",
+        des: "> Save data from your .gold program to the JS file."
+    },
+    {
+        name: "run",
+        des: "> Run your pre-determined program."
+    },
+    {
+        name: "delete",
+        des: "> Clear the current Program JS File."
+    },
+    {
+        name: "exit",
+        des: "> Exit the shell."
+    },
+    {
+        name: "github",
+        des: "> Opens the github repository in a new tab."
+    },
+    {
+        name: "author",
+        des: "> Opens Colack's Github link."
+    }
+];
+
+export { shellCommands };
