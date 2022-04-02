eaff625823a353f9dee808641466ad054030334b
Jack Spencer <UniformKitten281@gmail.com>
2022-04-01 21:47:57 -0700

    Create run.js

---
+++ modules/scripts/boot/run.js
@@ -0,0 +1,4 @@
+import { goldLoad } from "../../interpreters/goldInt.js";
+import { myGoldProgramArea, destination, currentError, goldExtensionCheck } from '../../util/index.js';
+
+goldLoad(destination);
