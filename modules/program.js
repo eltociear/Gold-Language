780e9d127710a29567e868c301495ddd13c4d664
Jack Spencer <UniformKitten281@gmail.com>
2022-04-01 21:43:40 -0700

    Create program.js

---
+++ modules/program.js
@@ -0,0 +1,16 @@
+var myGoldProgram = {
+    name: "Example Gold Program",
+    des: "An Example Gold Program for the README file.",
+    ex: [
+        {
+            fun: "log",
+            val1: "Hello World!",
+            val2: "",
+            logvaluetrue: "",
+            logvaluefalse: "",
+            type: ""
+        }
+    ]
+};
+
+export { myGoldProgram };
