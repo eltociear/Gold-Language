b07659b97da330b2095565fe31bc027dcf5903ae
Jack Spencer <UniformKitten281@gmail.com>
2022-04-03 19:44:36 -0700

    Delete run.js

--- modules/scripts/boot/run.js
+++
@@ -1,4 +0,0 @@
-import { goldLoad } from "../../interpreters/goldInt.js";
-import { myGoldProgramArea, destination, currentError, goldExtensionCheck } from '../../util/index.js';
-
-goldLoad(destination);
