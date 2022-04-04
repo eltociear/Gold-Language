4772e2f2ff6566cd649b1bc304d645737977ae4b
Jack Spencer <UniformKitten281@gmail.com>
2022-04-03 19:48:14 -0700

    Update program.js

--- modules/program.js
+++ modules/program.js
@@ -1,16 +1,2 @@
-var myGoldProgram = {
-    name: "Example Gold Program",
-    des: "An Example Gold Program for the README file.",
-    ex: [
-        {
-            fun: "log",
-            val1: "Hello World!",
-            val2: "",
-            logvaluetrue: "",
-            logvaluefalse: "",
-            type: ""
-        }
-    ]
-};
-
+var myGoldProgram; 
 export { myGoldProgram };
