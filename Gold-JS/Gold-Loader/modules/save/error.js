c8a81ebaa6147b7f966083fa3b00718e8e756bd7
Jack Spencer <UniformKitten281@gmail.com>
2022-04-10 18:54:29 -0700

    Create error.js

---
+++ Gold-JS/Gold-Loader/modules/save/error.js
@@ -0,0 +1,24 @@
+const errorCodes = [
+    {
+        code: 0,
+        des: "INVALID_ERROR"
+    },
+    {
+        code: 1,
+        des: "File does not end with .gold!"
+    },
+    {
+        code: 2,
+        des: "Type does not exist!"
+    },
+    {
+        code: 3,
+        des: "Invalid Response!"
+    },
+    {
+        code: 4,
+        des: "Egg slot is already taken up!"
+    }
+];
+
+export { errorCodes };
