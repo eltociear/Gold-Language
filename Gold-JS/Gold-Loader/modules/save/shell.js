9ac18c2db1711eb47c9d01a6d7ffec8dee656b02
Jack Spencer <UniformKitten281@gmail.com>
2022-04-10 18:54:38 -0700

    Create shell.js

---
+++ Gold-JS/Gold-Loader/modules/save/shell.js
@@ -0,0 +1,48 @@
+const shellCommands = [
+    {
+        name: "help",
+        des: "> Search for help on any type of command."
+    },
+    {
+        name: "exit / quit / leave",
+        des: "> Exit the shell."
+    },
+    {
+        name: "github",
+        des: "> Opens the github repository in a new tab."
+    },
+    {
+        name: "author",
+        des: "> Opens Colack's Github link."
+    },
+    {
+        name: "reload",
+        des: "> Reloads the entire console."
+    },
+    {
+        name: "amIOn",
+        des: "> Checks to see if the console is on."
+    },
+    {
+        name: "credits",
+        des: "> Shows the credits for the console."
+    },
+    {
+        name: "clear",
+        des: "> Clears the console."
+    },
+    {
+        name: "ty",
+        des: "> Thanks the console."
+    },
+    {
+        name: "motd",
+        des: "> Displays current Message of the Day."
+    },
+    {
+        name: "helloWrd",
+        des: "> Displays Hello World in the Console."
+    }
+];
+
+export { shellCommands };
